543a22fb311fe9c10084d180dd9e63d0912e33c3
Peter Kapteyn <peter.kapteyn@dornerworks.com>
2021-06-15 16:21:15 -0400

    add asdf

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -97,57 +97,63 @@ function Get-ChildItemUnixSimple {
 }
 
 
-function Get-ChildItemUnix {
-    $items = Get-ChildItem
-    [System.Collections.ArrayList]$items  = $items.name
-    $max_cols = $items.Count
-    $num_cols = 0
-    $num_rows = 0
-
-    # try out all row/col configs to find the one that fits
-    # ie the config that maximizes cols
-    for ($i = $max_cols; $i -ge 1; $i--){
-        $num_cols = $i
-        $num_rows = [Math]::Ceiling($items.Count / $num_cols)
-        $c = 0
-        $array = New-Object 'string[,]' $num_rows, $num_cols
-        $col_widths = @(0) * $num_cols
-
-        # fit the directory items into the array with this row/col config
-        for ($row = 0; $row -lt $num_rows; $row++){
-            for ($col = 0; $col -lt $num_cols; $col++){
-                try {
-                    $array[$row, $col] = $items[$c] + "  "
-
-                    # keep track of the widths so you know the largest width in each colom
-                    if ($array[$row, $col].Length -gt $col_widths[$col]) {
-                        $col_widths[$col] = $array[$row, $col].Length
-                    }
-                } catch {}
-                $c = $c + 1
-            }
-        }
-
-        # check if this row/col config will fit in the console horizontally
-        $console_len = $Host.UI.RawUI.WindowSize.Width
-        $total_row_len = 0
-        foreach($width in $col_widths) { $total_row_len = $total_row_len + $width }
-        if ($total_row_len -lt $console_len) {
-            break
-        }
+function Get-ChildItemUnix ($args) {
+    if ($args -eq "-l")
+    {
+        Get-ChildItem
     }
-
-    # print it to console
-    for($i = 0; $i -lt $num_rows; $i++){
-        for($j = 0; $j -lt $num_cols; $j++){
-            Write-Host $array[$i, $j] -NoNewline
-            $diff = $col_widths[$j] - $array[$i, $j].Length 
-            if($diff -gt 0) {
-                $diffstr = " " * $diff
-                Write-Host $diffstr -NoNewline
+    else {
+        $items = Get-ChildItem
+        [System.Collections.ArrayList]$items  = $items.name
+        $max_cols = $items.Count
+        $num_cols = 0
+        $num_rows = 0
+    
+        # try out all row/col configs to find the one that fits
+        # ie the config that maximizes cols
+        for ($i = $max_cols; $i -ge 1; $i--){
+            $num_cols = $i
+            $num_rows = [Math]::Ceiling($items.Count / $num_cols)
+            $c = 0
+            $array = New-Object 'string[,]' $num_rows, $num_cols
+            $col_widths = @(0) * $num_cols
+    
+            # fit the directory items into the array with this row/col config
+            for ($row = 0; $row -lt $num_rows; $row++){
+                for ($col = 0; $col -lt $num_cols; $col++){
+                    try {
+                        $array[$row, $col] = $items[$c] + "  "
+    
+                        # keep track of the widths so you know the largest width in each colom
+                        if ($array[$row, $col].Length -gt $col_widths[$col]) {
+                            $col_widths[$col] = $array[$row, $col].Length
+                        }
+                    } catch {}
+                    $c = $c + 1
+                }
+            }
+    
+            # check if this row/col config will fit in the console horizontally
+            $console_len = $Host.UI.RawUI.WindowSize.Width
+            $total_row_len = 0
+            foreach($width in $col_widths) { $total_row_len = $total_row_len + $width }
+            if ($total_row_len -lt $console_len) {
+                break
             }
         }
-        Write-host ""
+    
+        # print it to console
+        for($i = 0; $i -lt $num_rows; $i++){
+            for($j = 0; $j -lt $num_cols; $j++){
+                Write-Host $array[$i, $j] -NoNewline
+                $diff = $col_widths[$j] - $array[$i, $j].Length 
+                if($diff -gt 0) {
+                    $diffstr = " " * $diff
+                    Write-Host $diffstr -NoNewline
+                }
+            }
+            Write-host ""
+        }
     }
 }
 
