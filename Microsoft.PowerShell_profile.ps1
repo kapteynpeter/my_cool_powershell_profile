7a5950c76fa4ac2105f82ab8975e137b185d93d9
Peter Kapteyn <kapteynpeter@gmail.com>
2021-07-11 01:05:36 -0400

    asdf

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,14 @@
-. "C:\Users\peterkapteyn\Documents\WindowsPowerShell\ls.ps1"
-. "C:\Users\peterkapteyn\Documents\WindowsPowerShell\assorted_helpers.ps1"
-. "C:\Users\peterkapteyn\Documents\WindowsPowerShell\git-aliases.ps1"
-. "C:\Users\peterkapteyn\Documents\WindowsPowerShell\location-aliases.ps1"
-# . "C:\Users\peterkapteyn\Documents\WindowsPowerShell\print_string.ps1"
+$profile_path = $profile.substring(0, $profile.length - 33)
+
+. ($profile_path + "\ls.ps1")
+. ($profile_path + "\assorted_helpers.ps1")
+. ($profile_path + "\git-aliases.ps1")
+. ($profile_path + "\print_string.ps1")
 
 
 Remove-Item alias:ls -Force
 New-Alias -Name ls -Value Get-ChildItemUnix
 New-Alias -Name ll -Value Get-ChildItem
 
-Import-Module oh-my-posh
-Set-PoshPrompt -Theme stelbent.minimal
+# Import-Module oh-my-posh
+# Set-PoshPrompt -Theme stelbent.minimal
